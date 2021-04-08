a47c1468356fd37eb7936c9706090c366eb2b2f0
Abdulrahman Yusuf <abdulrahmanyusuf125@gmail.com>
2021-04-08 18:00:19 +0100

    format handleRedirect controller

--- src/controllers/handleRedirect.js
+++ src/controllers/handleRedirect.js
@@ -1,15 +1,15 @@
 const { database } = require("../lib");
 
 module.exports = async (request) => {
-	// According to the instructions given, short URL path should be of length 6 characters, so path cannot be more than 6.
-	if (request.params.code.length > 6) {
-		throw new Error("Invalid Short URL")
-	} else {
-		const url = await database.findURLByCode(request.params.code);
-	  if (url) {
-	    return { redirect: url.longurl };
-	  } else {
-	    throw new Error("Short URL does not exist");
-	  }
-	}
+  // According to the instructions given, short URL path should be of length 6 characters, so path cannot be more than 6.
+  if (request.params.code.length > 6) {
+    throw new Error("Invalid Short URL");
+  } else {
+    const url = await database.findURLByCode(request.params.code);
+    if (url) {
+      return { redirect: url.longurl };
+    } else {
+      throw new Error("Short URL does not exist");
+    }
+  }
 };
