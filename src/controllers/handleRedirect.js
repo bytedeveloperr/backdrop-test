5096cb1bdbf7c19f0912e0ba8e8999bea7402f92
Yusuf Abdulrahman <abdulrahmanyusuf125@gmail.com>
2021-04-08 17:50:48 +0100

    Update handleRedirect.js

--- src/controllers/handleRedirect.js
+++ src/controllers/handleRedirect.js
@@ -1,7 +1,7 @@
 const { database } = require("../lib");
 
 module.exports = async (request) => {
-	// short URL shouldn't be greater than 6 characters according to the instructions given.
+	// According to the instructions given, short URL path should be of length 6 characters, so path cannot be more than 6.
 	if (request.params.code.length > 6) {
 		throw new Error("Invalid Short URL")
 	} else {
