abf0fc7924085e884c02a9682dd3b9f163d11184
Abdulrahman Yusuf <abdulrahmanyusuf125@gmail.com>
2021-04-08 11:16:23 +0100

    update redirect controller

--- src/controllers/handleRedirect.js
+++ src/controllers/handleRedirect.js
@@ -1,10 +1,15 @@
 const { database } = require("../lib");
 
 module.exports = async (request) => {
-  const url = await database.findURLByCode(request.params.code);
-  if (url) {
-    return { redirect: url.longurl };
-  } else {
-    throw new Error("Short URL does not exist");
-  }
+	// short URL shouldn't be greater than 6 characters according to the instructions given.
+	if (request.params.code.length > 6) {
+		throw new Error("Invalid Short URL")
+	} else {
+		const url = await database.findURLByCode(request.params.code);
+	  if (url) {
+	    return { redirect: url.longurl };
+	  } else {
+	    throw new Error("Short URL does not exist");
+	  }
+	}
 };
